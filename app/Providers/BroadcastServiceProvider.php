445c65800d2eaa7e7003d8a624de9a052ec39262
Pierre Borgmann <drweissbrot@outlook.com>
2018-03-26 23:26:55 +0200

    synchronize drawing

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -2,20 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\ServiceProvider;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Broadcast::routes();
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot()
+	{
+		Broadcast::routes([
+			// TODO default middleware group is web -- CSRF tokens?
+			'middleware' => 'guest',
+		]);
 
-        require base_path('routes/channels.php');
-    }
+		require base_path('routes/channels.php');
+	}
 }
--- config/logging.php
+++ config/logging.php
@@ -1,71 +1,78 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Default Log Channel
-    |--------------------------------------------------------------------------
-    |
-    | This option defines the default log channel that gets used when writing
-    | messages to the logs. The name specified in this option should match
-    | one of the channels defined in the "channels" configuration array.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Default Log Channel
+	|--------------------------------------------------------------------------
+	|
+	| This option defines the default log channel that gets used when writing
+	| messages to the logs. The name specified in this option should match
+	| one of the channels defined in the "channels" configuration array.
+	|
+	 */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+	'default' => env('LOG_CHANNEL', 'stack'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Log Channels
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "custom", "stack"
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Log Channels
+	|--------------------------------------------------------------------------
+	|
+	| Here you may configure the log channels for your application. Out of
+	| the box, Laravel uses the Monolog PHP logging library. This gives
+	| you a variety of powerful log handlers / formatters to utilize.
+	|
+	| Available Drivers: "single", "daily", "slack", "syslog",
+	|                    "errorlog", "custom", "stack"
+	|
+	 */
 
-    'channels' => [
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => [
+	'channels' => [
+		'stack' => [
+			'driver' => 'stack',
+			'channels' => [
 				'single', 'discord_notice', 'discord_error',
 			],
-        ],
+		],
 
-        'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-        ],
+		'debug' => [
+			'driver' => 'stack',
+			'channels' => [
+				'single', 'discord_debug',
+			],
+		],
 
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 7,
-        ],
+		'single' => [
+			'driver' => 'single',
+			'path' => storage_path('logs/laravel.log'),
+			'level' => 'debug',
+		],
 
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
-        ],
+		'daily' => [
+			'driver' => 'daily',
+			'path' => storage_path('logs/laravel.log'),
+			'level' => 'debug',
+			'days' => 7,
+		],
 
-        'syslog' => [
-            'driver' => 'syslog',
-            'level' => 'debug',
-        ],
+		'slack' => [
+			'driver' => 'slack',
+			'url' => env('LOG_SLACK_WEBHOOK_URL'),
+			'username' => 'Laravel Log',
+			'emoji' => ':boom:',
+			'level' => 'critical',
+		],
 
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
-        ],
+		'syslog' => [
+			'driver' => 'syslog',
+			'level' => 'debug',
+		],
+
+		'errorlog' => [
+			'driver' => 'errorlog',
+			'level' => 'debug',
+		],
 
 		'discord_debug' => [
 			'driver' => 'custom',
@@ -114,7 +121,7 @@ return [
 			'via' => App\Logging\CreateDiscordEmergencyLogger::class,
 			'level' => 'emergency',
 		],
-    ],
+	],
 
 	'discord' => [
 		'debug' => env('DISCORD_WEBHOOK_DEBUG'),
